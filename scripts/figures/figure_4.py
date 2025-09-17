b7b980a068bb00b28fc097b1e87d06e63ad97a68
Michael <mpreston@ucsd.edu>
2025-09-17 16:41:14 -0700

    remove unused imoprts

--- scripts/figures/figure_4.py
+++ scripts/figures/figure_4.py
@@ -15,8 +15,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import seaborn as sns
-from statannotations.Annotator import Annotator
-from scipy.stats import ttest_ind, f_oneway
+from scipy.stats import f_oneway
 
 import sys
 sys.path.append("code")
