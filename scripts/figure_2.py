ac51ca4b4df5a67bbae1a123dd285b0b39daf4e9
Michael <mpreston@ucsd.edu>
2025-09-17 11:58:55 -0700

    drop figure 2 (3 species)

--- scripts/figure_2.py
+++ scripts/figure_2.py
@@ -1,5 +1,5 @@
 """
-Figure 2: Electrophysiological recordings from several species of fungi.
+Figure 1: Electrophysiological recording from fungal mycelial network.
 """
 
 # imports - standard
@@ -8,28 +8,26 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 from neurodsp.spectral import compute_spectrum
-from specparam import SpectralModel
-import numpy as np
-from scipy.stats import zscore
 from matplotlib.gridspec import GridSpec
 
 # imports - custom
 import sys
 sys.path.append("code")
-from info import FS, SPECIES
+from info import FS
 from utils import get_start_time, print_time_elapsed, beautify_ax
 
 # settings
-NPERSEG = 128 # samples per segment for fft
-TITLES = ('Herecium', 'Lentinus', 'Pleurotus')
-COLORS = sns.color_palette('colorblind')
+SPECIES = 'herecium' # species to plot
+NPERSEG = 64 # samples per segment for fft
+N_SAMPLES = 3000 # number of samples to plot
+COLORS = [sns.color_palette('colorblind')[0], 'grey'] # [mycelium, control]
 plt.style.use('mplstyle/default.mplstyle')
 
 
 def main():
 
     # display progress
-    print("\n\nGenerating Figure 2...")
+    print("\n\nGenerating Figure 1...")
     t_start = get_start_time()
 
     # identify / create directories
@@ -40,82 +38,44 @@ def main():
     # load data
     df = pd.read_csv('data/recordings.csv')
 
-    # Analysis #################################################################
+    # compute power spectrum
+    freqs, psd = compute_spectrum(df[SPECIES], FS, nperseg=NPERSEG)
+    _, psd_ctrl = compute_spectrum(df['control'], FS, nperseg=NPERSEG)
 
-    # init
-    psd = {}
-    models = {}
-    params_list = []
-
-    # analyze each species
-    for species in SPECIES[:3]:
-        # compute power spectrum
-        freqs, psd[species] = compute_spectrum(df[species], FS, nperseg=NPERSEG)
-
-        # fit spectral model
-        model = SpectralModel(aperiodic_mode='knee', verbose=False)
-        model.fit(freqs, psd[species])
-        models[species] = model
-        params = model.get_params('aperiodic')
-        temp = pd.Series({'species': species, 'offset': params[0],
-                        'knee': params[1], 'exponent': params[2]})
-        params_list.append(temp)
-    params = pd.DataFrame(params_list)
-
-    # normalize power
-    psd_norm = {}
-    fit_norm = {}
-    for species in SPECIES[:3]:
-        psd_norm[species] = psd[species] / np.sum(psd[species])
-        fit_norm[species] = 10**models[species]._ap_fit / np.sum(10**models[species]._ap_fit)
-        df[f"{species}_norm"] = zscore(df[species])
-        
-    # print results
-    print(params)
-    print(f"\nMean exponent: {params.loc[:2, 'exponent'].mean()}")
-
-    # Plotting #################################################################
-
-    # create figure 
+    # create figure
     fig = plt.figure(figsize=(6.5, 2.34), constrained_layout=True)
-    gs = GridSpec(3, 2, figure=fig, width_ratios=[1, 0.6], 
-                height_ratios=[1, 1, 1])
-    ax_a0 = fig.add_subplot(gs[0, 0])
-    ax_a1 = fig.add_subplot(gs[1, 0])
-    ax_a2 = fig.add_subplot(gs[2, 0])
-    ax_b = fig.add_subplot(gs[:, 1])
+    gs = GridSpec(2, 2, figure=fig, width_ratios=[1, 0.6], height_ratios=[1, 1])
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[1, 0])
+    ax3 = fig.add_subplot(gs[:, 1])
 
-    # A: time-series
-    for i, (sp, title, ax) in enumerate(zip(SPECIES[:3], TITLES, 
-                                            [ax_a0, ax_a1, ax_a2])):
-        ax.plot(df['time'], df[f"{sp}_norm"], 
-                c=COLORS[i], lw=1)
+    # A: plot time-series
+    titles = ('Mycelial network', 'Control')
+    for i, (column, title, ax) in enumerate(zip(df.columns[1:], titles, 
+                                                [ax1, ax2])):
+        ax.plot(df['time'][:N_SAMPLES], df[column][:N_SAMPLES], c=COLORS[i])
+        ax.set_ylabel("voltage (uV)")
         ax.set_title(title)
-    ax_a2.set_xlabel('time (s)')
-    fig.text(-0.01, 0.20, "normalized voltage (z-score)", rotation=90, fontsize=10)
+    ax2.set_xlabel('time (s)')
 
-    for ax in [ax_a0, ax_a1]:
-        ax.set_xticks([])
-
-    # B: plot power spectrum and fit
-    for sp, color in zip(SPECIES[:-1], COLORS):
-        ax_b.loglog(freqs, psd_norm[sp], color=color, lw=2, alpha=1, label=sp)
-        ax_b.plot(models[sp].freqs, fit_norm[sp], color=color, lw=2, 
-                linestyle='--', alpha=0.5)
-    ax_b.set(xlabel= 'frequency (Hz)', ylabel='normalized power (au)')
-    ax_b.set_title('Power spectral density')
-    ax_b.legend()
-
-    # beautify
-    for ax in [ax_a0, ax_a1, ax_a2, ax_b]:
-        beautify_ax(ax)
+    # B: plot power spectrum
+    ax3.loglog(freqs, psd, color=COLORS[0], lw=2, label='mycelium')
+    ax3.loglog(freqs, psd_ctrl, color=COLORS[1], lw=2, label='control')
+    ax3.set(xlabel= 'frequency (Hz)', ylabel='power (\u03BCV\u00b2/Hz)')
+    ax3.set_title('Power spectrum')
+    ax3.legend()
 
     # add panel labels
     fig.text(0.01, 0.95, 'a', fontsize=12, fontweight='bold')
-    fig.text(0.61, 0.95, 'b', fontsize=12, fontweight='bold')
+    fig.text(0.01, 0.50, 'b', fontsize=12, fontweight='bold')
+    fig.text(0.62, 0.95, 'c', fontsize=12, fontweight='bold')
+
+    # beautify axes
+    for ax in [ax1, ax2, ax3]:
+        beautify_ax(ax)
 
     # save
-    fig.savefig('figures/figure_2.png', bbox_inches='tight')
+    fig.savefig('figures/figure_1.png')
 
     # display progress
     print(f"\n\nTotal analysis time:")
