5b3b91ae37f0956d035473ca98138043ddefd134
Michael <mpreston@ucsd.edu>
2025-09-17 15:57:14 -0700

    update paths

--- code/info.py
+++ code/info.py
@@ -5,6 +5,3 @@ Info and dataset details.
 # Dataset details for each kingdom
 KINGDOMS = ['fungi', 'plantae', 'animalia']
 FS = dict({'fungi': 10, 'plantae': 30, 'animalia': 200}) # Sampling frequency (Hz)
-
-# path to Mishra et. al, 2024 dataset (https://zenodo.org/records/12812074)
-DATASET_PATH = '../../datasets' # ! SET PATH FOR YOUR SYSTEM !
