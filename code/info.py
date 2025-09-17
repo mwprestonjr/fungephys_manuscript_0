9356ae57e29b16b7a1db460ae805038dace561b2
Michael <mpreston@ucsd.edu>
2025-09-17 13:06:46 -0700

    add kingdom info to info.py

--- code/info.py
+++ code/info.py
@@ -1,10 +1,10 @@
 """
-Dataset details.
+Info and dataset details.
 """
+
+# Dataset details for each kingdom
+KINGDOMS = ['fungi', 'plantae', 'animalia']
+FS = dict({'fungi': 10, 'plantae': 30, 'animalia': 200}) # Sampling frequency (Hz)
 
 # path to Mishra et. al, 2024 dataset (https://zenodo.org/records/12812074)
 DATASET_PATH = '../../datasets' # ! SET PATH FOR YOUR SYSTEM !
-
-FS = 10 # sampling frequency in Hz (both datasets)
-SPECIES = ('herecium', 'lentinus', 'pleurotus', 'control')
-
