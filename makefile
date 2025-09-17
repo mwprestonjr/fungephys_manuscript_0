d21270e70a7c298bcbddb0d9fb9d0a58a392f946
Michael <mpreston@ucsd.edu>
2025-09-17 13:24:20 -0700

    add spectral analysis

--- makefile
+++ makefile
@@ -2,6 +2,7 @@ analysis:
 	python scripts/analysis/1a_epoch_fungi_data.py
 	python scripts/analysis/1b_epoch_plantae_data.py
 	python scripts/analysis/1c_epoch_animalia_data.py
+	python scripts/analysis/2_spectral_analysis.py
 
 figures:
 	python scripts/figures/figure_2.py
---
+++ scripts/analysis/2_spectral_analysis.py
@@ -0,0 +1,43 @@
+"""
+2_spectral_analysis.py
+===================
+Compute spectral power.
+
+"""
+
+# imports - standard
+import os
+import numpy as np
+from neurodsp.spectral import compute_spectrum
+
+# imports - custom
+import sys
+sys.path.append("code")
+from time_utils import get_start_time, print_time_elapsed
+from info import KINGDOMS, FS
+from settings import NPERSEG
+
+
+def main():
+    # display progress
+    t_start = get_start_time()
+
+    # identify / create directories
+    dir_output = f"data/spectra"
+    os.makedirs(dir_output, exist_ok=True)
+
+    # load data, compute spectra, and save
+    for kindom in KINGDOMS:
+        signals = np.load(f"data/epochs/{kindom}/signals.npy")
+        freqs, spectra = compute_spectrum(signals, FS[kindom], nperseg=NPERSEG)
+
+        np.save(f"{dir_output}/spectra_{kindom}.npy", spectra)
+        np.save(f"{dir_output}/freqs_{kindom}.npy", freqs)
+
+    # display progress
+    print(f"\n\nTotal analysis time:")
+    print_time_elapsed(t_start)
+
+
+if __name__ == "__main__":
+    main()
