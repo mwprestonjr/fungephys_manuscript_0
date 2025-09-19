3e476d3e57ceee60d11e397ee90c3f13b40c3268
Michael <mpreston@ucsd.edu>
2025-09-19 13:14:05 -0700

    switch figure order

--- scripts/figures/figure_3.py
+++ scripts/figures/figure_3.py
@@ -1,143 +1,196 @@
 """
-Figure 3. Reanalysis of electrophysiological recordings from Mishra et al., 2024.
+Figure 3: Shared spectral signiture across kingdoms
+
+A. Cartoon to represent kingdoms
+B. Example time-series for each kingdom
+C. Power spectra for each kingdom
+D. Violin plot of exponent across kingdoms 
+E. Violin plot of timescale across kingdoms 
 """
 
-# Imports - standard
+# imports
 import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import seaborn as sns
-from neurodsp.spectral import compute_spectrum
-from specparam import SpectralModel
-from matplotlib.gridspec import GridSpec
+from scipy.stats import f_oneway
 
-# Imports - custom
 import sys
 sys.path.append("code")
 from time_utils import get_start_time, print_time_elapsed
-from info import FS
-from settings import SPECPARAM_SETTINGS
-from plots import beautify_ax
+from info import KINGDOMS
+from utils import shift_signals, zscore
+from plots import plot_spectra, beautify_ax
 
 # settings
 plt.style.use('mplstyle/default.mplstyle')
-COLORS = sns.color_palette('colorblind')[:3]
-NPERSEG = 512 # samples per segment for fft
-SP_FIT_RANGE = [0, .5] # frequency range for spectral parameterization
-AP_MODE = 'knee' # aperiodic mode for spectral parameterization
-EPOCHS = {'low': (0, 1.5), 'high': (3.5, 5), 'control': (7, 8.5)}
-fs = FS['fungi']
+SHIFT = [8, 8, 5] # Shift signals for plotting (STDs)
+N_SIGNALS = 10 # Number of signals to plot per kingdom
+N_SAMPLES = {'fungi'   : 12000, 
+             'plantae' : 36000, 
+             'animalia': 1000} # Number of samples to plot
 
 
 def main():
 
     # display progress
-    print("\n\nGenerating Figure 3...")
+    print("\n\nGenerating Figure 4...")
     t_start = get_start_time()
 
     # identify / create directories
-    dir_output = "figures/main_figures"
-    if not os.path.exists(dir_output): 
-        os.makedirs(dir_output)
+    dir_output = f"figures/main_figures"
+    os.makedirs(dir_output, exist_ok=True)
 
-    # load data
-    try:
-        fname = "data/raw/mishra_2024/Light stimulation test/Mycelium_light test_plate 1.txt"
-        data = load_data(fname)
-    except FileNotFoundError:
-        print("Data not found. Please download the dataset from https://zenodo.org/records/12812074 and place in data/raw/mishra_2024/")
-        return
+    # LOAD DATA ################################################################
+    print("Importing data...")
 
-    # Analysis #################################################################
+    # init
+    signals, time, spectra, freqs = {}, {}, {}, {}
+    
+    # loop through kingdoms
+    for ii, kingdom in enumerate(KINGDOMS):
+        # load signals
+        signals_ii = np.load(f"data/epochs/{kingdom}/signals.npy")
+        time_ii = np.load(f"data/epochs/{kingdom}/time.npy")
+        signals[kingdom] = signals_ii[:N_SIGNALS, :N_SAMPLES[kingdom]]
+        time[kingdom] = time_ii[:N_SAMPLES[kingdom]]
 
-    # epoch data
-    data_0 = data[int(EPOCHS['control'][0]*3600*fs):int(EPOCHS['control'][1]*3600*fs)]
-    data_1 = data[int(EPOCHS['low'][0]*3600*fs):int(EPOCHS['low'][1]*3600*fs)]
-    data_2 = data[int(EPOCHS['high'][0]*3600*fs):int(EPOCHS['high'][1]*3600*fs)]
+        # load spectra
+        spectra[kingdom] = np.load(f"data/spectra/spectra_{kingdom}.npy")
+        freqs[kingdom] = np.load(f"data/spectra/freqs_{kingdom}.npy")
 
-    # compute spectra
-    freqs, spectra_0 = compute_spectrum(data_0, fs, nperseg=NPERSEG)
-    freqs, spectra_1 = compute_spectrum(data_1, fs, nperseg=NPERSEG)
-    freqs, spectra_2 = compute_spectrum(data_2, fs, nperseg=NPERSEG)
-
-    # apply specparam
-    model_0 = SpectralModel(**SPECPARAM_SETTINGS, aperiodic_mode=AP_MODE)
-    model_0.fit(freqs, spectra_0, freq_range=SP_FIT_RANGE)
-
-    model_1 = SpectralModel(**SPECPARAM_SETTINGS, aperiodic_mode=AP_MODE)
-    model_1.fit(freqs, spectra_1, freq_range=SP_FIT_RANGE)
-
-    model_2 = SpectralModel(**SPECPARAM_SETTINGS, aperiodic_mode=AP_MODE)
-    model_2.fit(freqs, spectra_2, freq_range=SP_FIT_RANGE)
-
-    # Plotting #################################################################
-    # init figure and gridspec
-    fig = plt.figure(figsize=(7.5, 2.5))
-    gs = GridSpec(1, 2, figure=fig, width_ratios=[1, 0.5])
-    ax_a = fig.add_subplot(gs[0, 0])
-    ax_b = fig.add_subplot(gs[0, 1])
-
-    # Panel a: Light stimulation, time series with time ranges annotated
-    ax_a.plot(np.arange(data.shape[0]) / fs / 3600, data, color='k', lw=0.5)
-    labels = ['control', 'UV (low)', 'UV (high)']
-    for ii, (key, (start, end)) in enumerate(EPOCHS.items()):
-        ax_a.axvspan(start, end, color=COLORS[ii], alpha=0.3, label=labels[ii])
-    ax_a.set(xlabel='time (hours)', ylabel='voltage (\u03BCV)')
-    ax_a.set_title('Light stimulation test')
-    ax_a.set_xlim(0, 10)
-    ax_a.legend()
-
-    # Panel b: Light stimulation, spectra fits
-    plot_sp_results(model_0, label='control', color=COLORS[0], ax=ax_b)
-    plot_sp_results(model_1, label='UV (low)', color=COLORS[1], ax=ax_b)
-    # plot_sp_results(model_2, label='UV (high)', color=COLORS[2], ax=ax_b)
-    ax_b.loglog(model_2.freqs, 10**model_2.power_spectrum, color=COLORS[2], lw=2, alpha=1, label='UV (high)')
-    ax_b.loglog(model_2.freqs, 10**model_2._ap_fit, color='k', linestyle='--', lw=1, alpha=1, label='model fits')
-    ax_b.set_title('Power spectra')
-    ax_b.legend()
+    # load spectral parameters
+    params = pd.read_csv("data/results/spectral_parameters.csv")
 
 
-    # beautify plot
-    for ax in [ax_a, ax_b]:
+    # STATS ####################################################################
+    print("\nRunning ANOVA...")
+    # exponent
+    exp_anova = f_oneway(params.loc[params['kingdom']=='animalia', 'exponent'],
+                         params.loc[params['kingdom']=='plantae', 'exponent'],
+                         params.loc[params['kingdom']=='fungi', 'exponent'])
+    print(f"\tExponent ANOVA: F={exp_anova.statistic:.2f}, p={exp_anova.pvalue:.3e}")
+
+    # timescale
+    ts_anova = f_oneway(params.loc[params['kingdom']=='animalia', 'timescale'],
+                        params.loc[params['kingdom']=='plantae', 'timescale'],
+                        params.loc[params['kingdom']=='fungi', 'timescale'])
+    print(f"\tTimescale ANOVA: F={ts_anova.statistic:.2f}, p={ts_anova.pvalue:.3e}")
+
+    # average parameters
+    print("\nParameter means:")
+    print(params.groupby('kingdom').mean()[['exponent', 'timescale']])
+
+    print("\nParameter stds:")
+    print(params.groupby('kingdom').std()[['exponent', 'timescale']])
+
+    # PLOT #####################################################################
+    print("\nPlotting...")
+    
+    # create figure and nested gridspec
+    fig = plt.figure(figsize=[6.5, 8], constrained_layout=True)
+    spec = gridspec.GridSpec(figure=fig, ncols=3, nrows=4, 
+                             width_ratios=[1, 1, 1], 
+                             height_ratios=[0.6, 0.6, 0.6, 1])
+    gs_a = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec=spec[:3, 0])
+    gs_de = gridspec.GridSpecFromSubplotSpec(1, 5, subplot_spec=spec[3, :],
+                                             width_ratios=[0.2, 1, 0.1, 1, 0.2])
+
+    # plot subplot a
+    ax_a = fig.add_subplot(gs_a[0])
+    ax_a.imshow(plt.imread("data/images/kingdom_cartoon.png"))
+    ax_a.axis('off')
+    fig.text(0.01, 0.90, 'Fungi', va='center', rotation='vertical', fontsize=12)
+    fig.text(0.01, 0.67, 'Plantae', va='center', rotation='vertical', fontsize=12)
+    fig.text(0.01, 0.43, 'Animalia', va='center', rotation='vertical', fontsize=12)
+
+    # loop through kingdoms 
+    for ii, kingdom in enumerate(KINGDOMS):
+
+        # plot subplot b
+        ax_b = fig.add_subplot(spec[ii, 1])
+        signals_i = prep_signal_for_plotting(signals[kingdom], std=SHIFT[ii])
+        ax_b.plot(time[kingdom], signals_i.T, color='k', linewidth=0.5)
+        ax_b.set(ylabel='voltage (z-score)')
+        ax_b.set_yticks([])
+        ax_b.spines['left'].set_visible(False)
+        beautify_ax(ax_b)
+
+        # plot subplot c
+        ax_c = fig.add_subplot(spec[ii, 2])
+        plot_spectra(spectra[kingdom], freqs[kingdom], ax=ax_c, color='k', title='')
+        ax_c.set_xlabel('')
+        beautify_ax(ax_c)
+
+        # labels
+        if ii == 0:
+            ax_b.set_title('Voltage time-series')
+            ax_c.set_title('Power spectra')
+            ax_c.set_ylabel('power ($mV^2/Hz$)') # fungi data in mV 
+
+        if ii == 2:
+            ax_b.set_xlabel('time (s)')
+            ax_c.set_xlabel('frequency (Hz)')
+
+    # plot subplot d
+    ax_d = fig.add_subplot(gs_de[1])
+    sns.boxplot(data=params, x='kingdom', y='exponent', ax=ax_d, color='gray')
+    labels =[label.get_text().capitalize() for label in ax_d.get_xticklabels()]
+    ax_d.set_xticks(ax_d.get_xticks(), labels)
+    ax_d.set(xlabel="kingdom", ylabel="exponent")
+    ax_d.set_title('Exponent')
+
+    # plot subplot e
+    ax_e = fig.add_subplot(gs_de[3])
+    sns.boxplot(data=params, x='kingdom', y='timescale', ax=ax_e, color='gray')
+    labels =[label.get_text().capitalize() for label in ax_e.get_xticklabels()]
+    ax_e.set_xticks(ax_e.get_xticks(), labels)
+    ax_e.set(xlabel="kingdom", ylabel="timescale")
+    ax_e.set_title('Timescale')
+    ax_e.set_ylabel('timescale (s)')
+    ax_e.set_yscale('log')
+
+    # beautify
+    for ax in [ax_d, ax_e]:
         beautify_ax(ax)
 
     # add panel labels
-    fig.text(0.03, 0.95, 'a', fontsize=12, fontweight='bold')
-    fig.text(0.70, 0.95, 'b', fontsize=12, fontweight='bold')
+    fig.text(0.28, 0.98, 'a', fontsize=12, fontweight='bold')
+    fig.text(0.68, 0.98, 'b', fontsize=12, fontweight='bold')
+    fig.text(0.11, 0.31, 'c', fontsize=12, fontweight='bold')
+    fig.text(0.57, 0.31, 'd', fontsize=12, fontweight='bold')
 
-    # save and show
-    fig.savefig(f'{dir_output}/figure_3.png')
-
-    # Print results ############################################################
-
-    # print exponent
-    print("Exponents:")
-    print(f"  Control: {model_0.get_params('aperiodic', 'exponent'):0.2f}")
-    print(f"  Treatment (low): {model_1.get_params('aperiodic', 'exponent'):0.2f}")
-    print(f"  Treatment (high): {model_2.get_params('aperiodic', 'exponent'):0.2f}")
+    # save figure
+    fig.savefig(f"{dir_output}/figure_3.png")
 
     # display progress
     print(f"\n\nTotal analysis time:")
     print_time_elapsed(t_start)
 
 
-def load_data(fname):
-    # import data
-    data = pd.read_csv(fname, sep=r'\s+', header=None, index_col=0,
-                       names=['voltage'])
-    data = np.squeeze(data.values)
+def prep_signal_for_plotting(signals, std):
+    """
+    Subtract mean, z-score and vertically shift signals for plotting.
 
-    # interpolate nan values 
-    data = pd.Series(data)
-    data = data.interpolate()
-    
-    return data.values
+    Parameters
+    ----------
+    signals : 2D array
+        Signal to shift.
+    std : int
+        Number of standard deviations to shift.
 
+    Returns
+    -------
+    signals : 2D array
+        Shifted signal.
+    """
 
-def plot_sp_results(model, label, color, ax):
-    ax.loglog(model.freqs, 10**model.power_spectrum, c=color, lw=2, label=label)
-    ax.loglog(model.freqs, 10**model._ap_fit, c='k', linestyle='--', lw=1)
+    signals = zscore(signals)
+    signals = shift_signals(signals, std=std)
+
+    return signals
 
 
 if __name__ == "__main__":
